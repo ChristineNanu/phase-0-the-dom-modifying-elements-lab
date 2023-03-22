525de5073797721b3350a7a8d3ae783dea4dce3a
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-03-22 19:36:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code here!
 
-document.querySelector('main').remove();
+document.querySelector('main').remove()
 
 const newHeader=document.createElement('h1');
+
  newHeader.id=("victory")
  newHeader.textContent="Mundi is the champion"
